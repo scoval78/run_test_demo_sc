db2bfe2df08d17000abe74fbd8c814023a6205f5
scoval78 <scoval78@gmail.com>
2026-05-27 23:02:46 +0300

    first action My fix

--- tests/test_button.py
+++ tests/test_button.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 
 @pytest.fixture()
 def browser():
-    chrome_browser = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    chrome_browser = webdriver.Chrome(options=options)
     return chrome_browser
 
 
 def test_button_exist(browser):
-    browser.maximize_window('https://www.qa-practice.com/elements/button/simple')
+    browser.get('https://www.qa-practice.com/elements/button/simple')
     assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
